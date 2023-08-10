01de2f7943539cdc13897698001df92f6079b2f1
Gracy222 <gbamidele23@gmail.com>
2023-08-10 07:14:27 -0700

    check

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -1,25 +1,24 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * check_cycle - Checks if a singly linked list has a cycle in it.
+ * @list: Pointer to the head of the linked list.
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
-typedef struct listint_s
+int check_cycle(listint_t *list)
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	listint_t *slow = list, *fast = list;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-#endif /* LISTS_H */
+		/* If slow and fast meet at the same node, there's a cycle */
+		if (slow == fast)
+			return (1);
+	}
+	/* No cycle found */
+	return (0);
+}
